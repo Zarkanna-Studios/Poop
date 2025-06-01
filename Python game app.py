f094a196aa78cc519d20c041afc0e404f53f22a5
Zarkanna-Studios <Zarkanna_Studios@outlook.com>
2025-05-31 22:43:01 -0500

    nnnn

--- Python game app.py
+++ Python game app.py
@@ -6,3 +6,10 @@ chicken_nugget = 5000
 hello = 1000
 def adsfdfasdfs():
     pass
+
+
+
+
+lweekrjfedfjhdsjklfasjkdfhashfkasjlhfdlksjh = 329748129380472138947290847219084720
+
+print("ladjfhsdlkjfhsdljkfhslkjfhjksfhlkjsfhsadlkfhaslkjfhadsjklfhadsjklfhsjklhttps://github.com/Zarkanna-Studios/Poop.githttps://github.com/Zarkanna-Studios/Poop.githttps://github.com/Zarkanna-Studios/Poop.githttps://github.com/Zarkanna-Studios/Poop.githttps://github.com/Zarkanna-Studios/Poop.githttps://github.com/Zarkanna-Studios/Poop.githttps://github.com/Zarkanna-Studios/Poop.githttps://github.com/Zarkanna-Studios/Poop.git")
